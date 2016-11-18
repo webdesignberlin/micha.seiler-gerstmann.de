baf0e531fee0e266fbd9b35bc96ce95e98f7e9ee
Michael Gerstmann <reg@seiler-gerstmann.de>
2016-11-18 13:21:02 +0100

    [improvements] prevent browser reflow if typewriter element goes wild

--- app/js/modules/typewriter.js
+++ app/js/modules/typewriter.js
@@ -3,6 +3,10 @@
 module.exports = function() {
   var typewriterElement = document.getElementById('js-typewriter');
   var text = typewriterElement.getAttribute('data-text');
+  var helperHeight = typewriterElement.offsetHeight;
+
+  /* Prevent Reflow if typewriterElement types over one line */
+  typewriterElement.style.minHeight = helperHeight + 'px';
 
   function typeWriter(text, n) {
     if (n < (text.length)) {
