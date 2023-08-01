d3fd8894b46eb1727071836dd969235bdd3825bd
Michael Raguse <michael.raguse@fitx.de>
2023-08-01 12:29:55 +0200

    cleanup

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,5 +1,7 @@
 import { mailProtection } from './modules/mail-protect.ts';
 import { writeCurrentDate } from './modules/age.ts';
+import { typeWriter } from './modules/typewriter.ts';
 
 writeCurrentDate('.js-age');
 mailProtection();
+typeWriter();
