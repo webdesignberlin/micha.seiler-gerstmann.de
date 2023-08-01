2f40fda7d138d36ec711bf50b2842ad5dc8c9403
Michael Raguse <michael.raguse@fitx.de>
2023-08-01 12:25:31 +0200

    optimize date

--- src/js/modules/date.ts
+++ src/js/modules/date.ts
@@ -1,22 +1,8 @@
-const checkDateNeedZeroPrefix = (date) => date < 10;
-const getFormatedDayOrMonth = (date) => {
-  if (checkDateNeedZeroPrefix(date)) {
-    return `0${date}`;
-  }
-  return date;
-};
-const today = new Date();
-const day = today.getDate();
-const month = today.getMonth() + 1; // cause January is 0
-const year = today.getFullYear();
-const todayFormated = `${year}-${getFormatedDayOrMonth(month)}-${getFormatedDayOrMonth(day)}`;
 
-
-function writeCurrentDate(targets) {
-  const selectors = document.querySelectorAll(targets);
+export const writeCurrentDate = (target: string) => {
+  const selectors = document.querySelectorAll(target);
   if (selectors) {
-    selectors.forEach(item => item.setAttribute('datetime', todayFormated));
+    selectors.forEach((item) => item
+      .setAttribute('datetime', new Intl.DateTimeFormat("fr-CA", {year: "numeric", month: "2-digit", day: "2-digit"}).format(Date.now())));
   }
 };
-
-writeCurrentDate('.js-current-date');
